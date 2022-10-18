2326b01b2604cb6b99e2416aaa51a96e35209504
shamspias <shamsuddinpias0@gmail.com>
2022-10-18 18:33:57 +0600

    model to store emails and replay data

--- email_replay/models.py
+++ email_replay/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class EmailWithReplays(models.Model):
+    """
+    Email model to store email and choices replay and other options as well
+    """
+    email_subject = models.CharField(max_length=250, blank=True, null=True)
+    email_body = models.TextField(blank=True, null=True)
+    replay = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        if self.email_body:
+            return self.email_body[:20]
+        else:
+            return "Email with Replay"
