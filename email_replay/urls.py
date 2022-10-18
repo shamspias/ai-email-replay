240f9d75057371fe54b773317d00b969754d5b6b
shamspias <shamsuddinpias0@gmail.com>
2022-10-18 18:34:49 +0600

    url to work with email replay view

---
+++ email_replay/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    EmailReplayGenViewSet
+)
+
+email_replay_router = DefaultRouter()
+email_replay_router.register('replay', EmailReplayGenViewSet)
