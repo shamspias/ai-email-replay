0c30d2550719917c2ebab45a8f8d0ef2c45866c1
shamspias <shamsuddinpias0@gmail.com>
2022-10-18 19:56:46 +0600

    replay, body serializers

--- email_replay/serializers.py
+++ email_replay/serializers.py
@@ -1,15 +1,62 @@
 from rest_framework import serializers
-from .models import EmailWithReplays
+from .models import EmailWithReplays, EmailBody, EmailReplay
 
 
-class GetEmailSerializers(serializers.ModelSerializer):
+class EmailBodySerializers(serializers.ModelSerializer):
     """
     Serializer class to get email
     """
 
     class Meta:
+        model = EmailBody
+        fields = [
+            'actor',
+            'body',
+        ]
+
+
+class EmailReplaySerializers(serializers.ModelSerializer):
+    """
+    Serializer class to get email
+    """
+
+    class Meta:
+        model = EmailReplay
+        fields = [
+            'title',
+            'body',
+        ]
+
+
+class EmailSerializers(serializers.ModelSerializer):
+    """
+    Serializer class to get email
+    """
+    body = EmailBodySerializers(read_only=True)
+    body_id = serializers.PrimaryKeyRelatedField(
+        queryset=EmailBody.objects.all(), source='body', write_only=True)
+
+    class Meta:
         model = EmailWithReplays
         fields = [
-            'email_subject',
-            'email_body',
+            'subject',
+            'body',
+            'body_id',
+
+        ]
+
+
+class GetEmailReplaySerializers(serializers.ModelSerializer):
+    """
+    Serializer class to get email
+    """
+    replay = EmailReplaySerializers(read_only=True)
+    replay_id = serializers.PrimaryKeyRelatedField(
+        queryset=EmailReplay.objects.all(), source='body', write_only=True)
+
+    class Meta:
+        model = EmailWithReplays
+        fields = [
+            'replay',
+            'replay_id',
         ]
